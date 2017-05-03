25c418d0792ee1dbb911a6c0b01a31378478863a
James <jbkuczma@buffalo.edu>
2017-05-03 14:31:36 -0400

    very rough implementation but photo is no pixel art

--- Pixel/PixelArt/pixelart.py
+++ Pixel/PixelArt/pixelart.py
@@ -1,18 +1,46 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 class PixelArt:
 
     def __init__(self, image):
-        self.image = image
+        self.image = Image.open(image).convert('RGB')
+        self.width = self.image.width
+        self.height = self.image.height
+        self.pixels = self.getPixels()
 
     def RGBtoHSL(self, color):
         r = color[0]
         g = color[1]
         b = color[2]
+        print(color)
 
     def HSLtoRGB(self, color):
         h = color[0]
         s = color[1]
         l = color[2]
+
+    def getPixels(self):
+        return list(self.image.getdata())
+
+    def saturateImage(self):
+        converter = ImageEnhance.Color(self.image)
+        self.image = converter.enhance(2.0)
+
+    def reduceNumberOfColorsInPalette(self, numberOfColors):
+        reduced = self.image.convert('P', palette=Image.ADAPTIVE, colors=numberOfColors)
+        self.image = reduced
+
+    def resizeImageWithPixel(self):
+        # change
+        resized = self.image.resize((75, 50), Image.ANTIALIAS)
+        self.image = resized
+
+    def resizeImageWithPercent(self, percent):
+        resized = self.image.resize((self.width, self.height), Image.ANTIALIAS)
+        self.image = resized
+
+    def save(self, filename):
+        self.image.save(filename)
+        
 
     
--- pixel_art_example.py
+++ pixel_art_example.py
@@ -2,4 +2,9 @@ from Pixel import PixelArt
 
 
 x = PixelArt('images/chicago_test.png')
+x.saturateImage()
+x.reduceNumberOfColorsInPalette(25)
+x.resizeImageWithPixel()
+x.resizeImageWithPercent(100)
+x.save('tester.png')
 print('good')
