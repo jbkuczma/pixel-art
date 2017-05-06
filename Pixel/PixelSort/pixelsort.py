1781c865767f751cc412efb6d5259dcb0da9f75a
James <jbkuczma@buffalo.edu>
2017-05-06 13:48:02 -0400

    sorting by brightness implemented. by black and white next

--- Pixel/PixelSort/pixelsort.py
+++ Pixel/PixelSort/pixelsort.py
@@ -1,3 +1,6 @@
+# python implementation of ASDFPixelSort by Kim Asendorf
+# found here: https://github.com/kimasendorf/ASDFPixelSort/blob/master/ASDFPixelSort.pde
+
 from PIL import Image
 
 class PixelSort:
@@ -18,20 +21,13 @@ class PixelSort:
         self.whiteValue = -13000000
 
     def sort(self):
-        #iterate over columns
-        # number of columns = len(self.pixels) / self.width
         self.sortColumns()
-        #iterate over rows
-        # number of rows = len(self.pixels) / self.height
         self.sortRows()
-        #save new image
-        print(len(self.pixels)/self.height)
 
     ### Column ###
     def sortColumns(self):
         column = 0
         index = 0
-        # len(self.width)
         while column < self.width:
             self.sortColumn(column)
             column+=1
@@ -39,10 +35,8 @@ class PixelSort:
     def sortColumn(self, startIndex):
         # current column
         x = startIndex
-
         # where to start sorting
         y = 0
-
         # where to stop sorting
         yEnd = 0
 
@@ -50,15 +44,12 @@ class PixelSort:
             if self.sortMethod == 0:
                 y = self.getFirstPixelNotBlackY(x, y)
                 yEnd = self.getNextBlackPixelY(x, y)
-                break
             elif self.sortMethod == 1:
                 y = self.getFirstBrightPixelY(x, y)
                 yEnd = self.getNextDarkPixelY(x, y)
-                break
             elif self.sortMethod == 2:
                 y = self.getFirstPixelNotWhiteY(x, y)
                 yEnd = self.getNextWhitePixelY(x, y)
-                break
             else:
                 break
 
@@ -67,7 +58,7 @@ class PixelSort:
             
             sortLength = yEnd - y
             pixelsInColumn = []
-            
+
             for i in range(0, sortLength):
                 pixelsInColumn.append(self.pixels[x + (y+i) * self.width])
             
@@ -83,7 +74,7 @@ class PixelSort:
             while self.pixels[x + y * self.width] < self.blackValue:
                y+=1
                if y >= self.height:
-                   return - 1
+                   return -1
         return y 
 
     def getNextBlackPixelY(self, x, y):
@@ -142,10 +133,8 @@ class PixelSort:
     def sortRow(self, startIndex):
         # current row
         y = startIndex
-
         # where to start
         x = 0
-
         # where to stop
         xEnd = 0
 
@@ -153,15 +142,12 @@ class PixelSort:
             if self.sortMethod == 0:
                 x = self.getFirstPixelNotBlackX(x, y)
                 xEnd = self.getNextBlackPixelX(x, y)
-                break
             elif self.sortMethod == 1:
                 x = self.getFirstBrightPixelX(x, y)
                 xEnd = self.getNextDarkPixelX(x, y)
-                break
             elif self.sortMethod == 2:
                 x = self.getFirstPixelNotWhiteX(x, y)
                 xEnd = self.getNextWhitePixelX(x, y)
-                break
             else:
                 break
             
@@ -180,7 +166,51 @@ class PixelSort:
                 self.pixels[x + i + y * self.width] = pixelsInRow[i]
             
             x = xEnd+1
+    
+    def getFirstPixelNotBlackX(self, x, y):
+        while self.pixels[x + y * self.width] < self.blackValue:
+            x+=1
+            if x >= self.width:
+                return -1
+        return x
 
+    def getNextBlackPixelX(self, x, y):
+        x+=1
+        while self.pixels[x + y * self.width] > self.blackValue:
+            x+=1
+            if x > self.width:
+                return self.width - 1
+        return x-1
+
+    def getFirstBrightPixelX(self, x, y):
+        while self.getBrightness(self.pixels[x + y * self.width]) < self.brightnessValue:
+            x+=1
+            if x >= self.width:
+                return -1
+        return x
+
+    def getNextDarkPixelX(self, x, y):
+        x+=1
+        while self.getBrightness(self.pixels[x + y * self.width]) > self.brightnessValue:
+            x+=1
+            if x >= self.width:
+                return self.width - 1
+        return x-1
+
+    def getFirstPixelNotWhiteX(self, x, y):
+        while self.pixels[x + y * self.width] > self.whiteValue:
+            x+=1
+            if x >= self.width:
+                return -1
+        return x
+
+    def getNextWhitePixelX(self, x, y):
+        x+=1
+        while self.pixels[x + y * self.width] < self.whiteValue:
+            x+=1
+            if x >= self.width:
+                self.width - 1
+        return x-1
 
     ### End of Row section ###
 
@@ -193,9 +223,18 @@ class PixelSort:
         g = pixel[1]
         b = pixel[2]
         return (r * 0.2126) + (g * 0.7152) + (b * 0.0722)
+    
+    def save(self, filename):
+        #create a new image with the same mode and dimensions as the original image
+        newImage = Image.new(self.image.mode, self.image.size)
+        #put the sorted pixels into the new image
+        newImage.putdata(self.pixels)
+        #save
+        newImage.save(filename)
 
-    def pixelSortImage(self, sortingMethod=1):
+    def pixelSortImage(self, outputFilename, sortingMethod=1):
         # set the sorting method
         self.sortMethod = sortingMethod
         self.sort()
+        self.save(outputFilename)
         print('Pixel Sorting complete!')
--- pixel_art_example.py
+++ pixel_art_example.py
@@ -1,9 +1,13 @@
 from Pixel import PixelArt
+from Pixel import PixelSort
 
 
 # x = PixelArt('images/chicago_test.png')
 # x.turnImageIntoPixelArt('tester.png')
 
-x = PixelArt('images/5.jpg')
-x.turnImageIntoPixelArt('tester2.png', numberOfColorsInPalette=20)
+# x = PixelArt('images/5.jpg')
+# x.turnImageIntoPixelArt('tester2.png', numberOfColorsInPalette=20)
+
+x = PixelSort('images/5.jpg')
+x.pixelSortImage('o7.png', sortingMethod=1)
 
